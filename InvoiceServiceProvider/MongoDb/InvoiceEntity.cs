6e48ab60ed79fe5f32889aca8da75aebfee18080
Felix Cau <130475694+Felix-Cau@users.noreply.github.com>
2025-05-19 14:01:18 +0200

    Added email property in proto file

--- InvoiceServiceProvider/MongoDb/InvoiceEntity.cs
+++ InvoiceServiceProvider/MongoDb/InvoiceEntity.cs
@@ -9,6 +9,7 @@ namespace InvoiceServiceProvider.MongoDb
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
+        public string Email { get; set; } = null!;
         public string Address { get; set; } = null!;
         public string PostalCode { get; set; } = null!;
         public string City { get; set; } = null!;
